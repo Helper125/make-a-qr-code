7afc685cd910b66d81de7afab5a604adbef2e12d
Helper125 <dimaridcenko269@gmail.com>
2024-12-24 16:00:18 +0200

    Added new changes to the code

--- qr-code.py
+++ qr-code.py
@@ -1,4 +1,4 @@
-import qrcode
+import qrcode #you need to download the library(pip install qrcode(it is to make a qr-code), pip install Pillow(it's to make an image))
 
 inp = input("enter what you want to insert into the qr-code")
 save = input("enter the name of the qr-code file")
